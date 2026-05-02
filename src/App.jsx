8010cd5a38ffa630081de5e1fd5746cfec4555bc
yashchakurkar22 <yash.chakurkar23@vit.edu>
2026-05-02 17:08:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import ProductList from "./ProductList";
 
 function App() {
+  const [showProductList, setShowProductList] = useState(false);
+
   return (
-    <div className="landing">
-      <h1>Paradise Nursery 🌿</h1>
-      <button>Get Started</button>
+    <div>
+      {!showProductList ? (
+        <div className="landing">
+          <h1>Welcome to Paradise Nursery</h1>
+          <button onClick={() => setShowProductList(true)}>
+            Get Started
+          </button>
+        </div>
+      ) : (
+        <ProductList />
+      )}
     </div>
   );
 }
